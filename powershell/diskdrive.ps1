7e10086ad1203dd51101aa836c0e93af6c1b6f21
Sahil Patel <sp610344@gmail.com>
2023-08-10 18:16:38 -0400

    removed uneccessary files

--- powershell/diskdrive.ps1
+++
@@ -1,4 +0,0 @@
-foreach ($disk in $diskdrives) {
-      $partitions = $disk|get-cimassociatedinstance -resultclassname CIM_diskpartition
-      $partitions
-}
--- powershell/helloworld.ps1
+++
@@ -1,2 +0,0 @@
-"Hello World!"
-"This Sahil's Script for COMP2101 PowerShell."
